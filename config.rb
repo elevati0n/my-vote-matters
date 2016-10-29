65a074474fe79e1fd5c62bd3486fd2adf232fc99
sean <s@a.c>
2016-10-29 04:02:09 -0400

    updated readme to walk through compass workflow and changes + benefits

--- config.rb
+++ config.rb
@@ -11,7 +11,8 @@ images_dir = "images"
 javascripts_dir = "js"
 
 # You can select your preferred output style here (can be overridden via the command line):
-output_style = :compressed
+output_style = :expanded
+# :compressed
 #:expanded 
 #or :nested or :compact or :compressed
 
@@ -19,7 +20,7 @@ output_style = :compressed
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-line_comments = false
+# line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
